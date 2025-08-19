cef1e35f7f92cfb99c8c255a94637613f5e1652c
hyoaru <jjcabreraaaa@gmail.com>
2025-08-19 08:43:27 +0800

    Updated certifications

--- staging/resize-image/resize-images.sh
+++
@@ -1,7 +0,0 @@
-if [[ ! -d 'resized' ]]; then
-  mkdir resized
-fi
-
-for filename in *.{png,jpg,jpeg}; do
-	convert "${filename}" -resize 1360x605\> -gravity north -extent 1360x605 "resized/${filename}"
-done
